9f57d7a91ee9f442d6c34917e19df81f30e1acab
tristanpadiou <padioutristan@gmail.com>
2024-10-02 22:12:50 -0400

    finalized some code to meet req

--- pipelinee.py
+++ pipelinee.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler, LabelEncoder
@@ -10,7 +10,8 @@ from imblearn.over_sampling import SMOTE
 # data cleaning function
 
 def cleaning(df):
-    df.drop_duplicates(keep='first', inplace=True)
+    
+    #df.drop_duplicates(keep='first', inplace=True)
     def quality(rating):
         if rating >= 7:
             return 'promising'
@@ -41,8 +42,23 @@ def training(df):
     smote=SMOTE(random_state=1, sampling_strategy='auto')
     X_resampled, y_resampled = smote.fit_resample(X_train_orig, y_train_orig)
 
+
+
+    # hyperparameter tuning
+    ## Running the model first
+    cc_model_tunning = RandomForestClassifier()
+    cc_model_tunning.fit(X_resampled, y_resampled)
+    #setting up gridsearch
+    param_grid={
+    'n_estimators':[50,100,200],
+    'max_depth':[None, 1,2,3,4,5,6,7,8,9,10,20,30]}
+    grid_search=GridSearchCV(estimator=cc_model_tunning,param_grid=param_grid, cv=5)
+    grid_search.fit(X_resampled,y_resampled)
+
     #   training
-    cc_model = RandomForestClassifier(max_depth=5)
+    cc_model = RandomForestClassifier()
+    #   setting the hyperparameters as per the gridsearch
+    cc_model.set_params(**grid_search.best_params_)
     cc_model.fit(X_resampled, y_resampled)
     cc_y_pred = cc_model.predict(X_test_orig)
 
