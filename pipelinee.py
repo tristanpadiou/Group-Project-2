0b79f6a30142958c14f7c4ef8179d7f2ced7f5ab
tristanpadiou <padioutristan@gmail.com>
2024-10-04 13:18:37 -0400

    final fine-tuning

--- pipelinee.py
+++ pipelinee.py
@@ -5,9 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import balanced_accuracy_score,accuracy_score
-from imblearn.combine import SMOTEENN
 from imblearn.over_sampling import SMOTE,RandomOverSampler
-from imblearn.under_sampling import ClusterCentroids,RandomUnderSampler
 
 # data cleaning function
 
@@ -45,14 +43,11 @@ def training(df):
     X_resampled, y_resampled = best_resampler.fit_resample(X_train_orig, y_train_orig)
 
     #   training
-    cc_model = RandomForestClassifier()
+    cc_model = RandomForestClassifier(random_state=42)
     #   setting the hyperparameters as per the gridsearch
     cc_model.set_params(**hyperparameter_tuning(X_resampled, y_resampled))
     cc_model.fit(X_resampled, y_resampled)
     cc_y_pred = cc_model.predict(X_test_orig)
-
-    print('accuracy_score of Random Forest Classifier:')
-    print(accuracy_score(y_test_orig, cc_y_pred))
     print('Classification report of Random Forest Classifier:')
     print(classification_report(y_test_orig, cc_y_pred, target_names=le.classes_))
 
@@ -60,12 +55,12 @@ def training(df):
 # resampling: creating an automation to pick the best sampling method
 
 def best_resampling_tech (X_train_orig, X_test_orig, y_train_orig, y_test_orig):
-    samplers=[RandomOverSampler(random_state=1),SMOTE(random_state=1, sampling_strategy='auto'),SMOTEENN(random_state=1),ClusterCentroids(random_state=1),RandomUnderSampler(random_state=1)]
+    samplers=[RandomOverSampler(random_state=1),SMOTE(random_state=1, sampling_strategy='auto')]
     Scores=[]
     for sampler in samplers:
         sample=sampler
         X_resampled, y_resampled = sample.fit_resample(X_train_orig, y_train_orig)
-        cc_model = RandomForestClassifier()
+        cc_model = RandomForestClassifier(random_state=42)
         cc_model.fit(X_resampled, y_resampled)
         cc_y_pred = cc_model.predict(X_test_orig)
         Scores.append([
@@ -75,6 +70,7 @@ def best_resampling_tech (X_train_orig, X_test_orig, y_train_orig, y_test_orig):
     sorted_scores = sorted(Scores, reverse=True)
     best_resampler = sorted_scores[0][1]
     print(f'resampling method used: {best_resampler}')
+    print('---------------------------------------------------------')
     return best_resampler
 
 
@@ -83,7 +79,7 @@ def best_resampling_tech (X_train_orig, X_test_orig, y_train_orig, y_test_orig):
 
 def hyperparameter_tuning(X_resampled, y_resampled):
     print('Performig hyperparameter-tuning with Gridsearch CV. Can take up to 1min 30sec')
-    cc_model_tunning = RandomForestClassifier()
+    cc_model_tunning = RandomForestClassifier(random_state=42)
     cc_model_tunning.fit(X_resampled, y_resampled)
     #setting up gridsearch
     param_grid={
@@ -91,4 +87,6 @@ def hyperparameter_tuning(X_resampled, y_resampled):
     'max_depth':[None, 1,2,3,4,5,6,7,8,9,10,20,30]}
     grid_search=GridSearchCV(estimator=cc_model_tunning,param_grid=param_grid, cv=5)
     grid_search.fit(X_resampled,y_resampled)
+    print(f'the best parameters are: {grid_search.best_params_}')
+    print('---------------------------------------------------------')
     return grid_search.best_params_
